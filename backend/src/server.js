bcb897494c6f4e139c578869a3a005477df0c109
Prajwal <prajwal77dwivedi@gmail.com>
2025-09-05 13:32:09 +0530

    Rate Limiter Functionality Added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 
 import notesRoutes from "./routes/notes.route.js";
 import { connectDB } from "../config/db.js";
+import rateLimiter from "../middleware/rateLimiter.js";
 
 dotenv.config();
 
@@ -11,9 +12,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
+app.use(rateLimiter); //use before defining routes to apply rate limiting
+
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-  connectDB();
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
