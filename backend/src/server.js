9d310f956b08aab20e2d7237e2ae88cb586ce70f
Prajwal <prajwal77dwivedi@gmail.com>
2025-09-06 15:37:46 +0530

    Deployment Ready

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { fileURLToPath } from "url";
+import path from "path";
+import { dirname } from "path";
 
 import notesRoutes from "./routes/notes.route.js";
 import { connectDB } from "../config/db.js";
@@ -10,18 +13,34 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 
 app.use(express.json());
 
 app.use(rateLimiter); //use before defining routes to apply rate limiting
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  console.log(
+    "Serving frontend from:",
+    path.join(__dirname, "../../frontend/dist")
+  );
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.get("*", (req, res) => {
+    console.log("Serving index.html for:", req.url);
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
